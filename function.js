435a84abaae3a5d53920f5606adc87d9693f2e7f
Danil gdj <danilguidjou7@gmail.com>
2024-06-13 14:21:59 +0100

    read button properties

--- function.js
+++ function.js
@@ -3,20 +3,24 @@ const dialog = document.querySelector("dialog")
 const title = document.querySelector("#title")
 const author = document.querySelector("#author")
 const nbrPage = document.querySelector("#nbrPage")
+const read = document.querySelector("#read")
 const exitDialog = document.querySelector("#exit")
 const displayCards = document.querySelector(".displayCard")
 const form = document.querySelector("form")
 
-function Book(title , author , nbrPage){
+function Book(title , author , nbrPage, read){
     this.title = title;
     this.author =author;
     this.nbrPage = nbrPage;
+    this.read = read
 }
 
 function addBookToLibrary(newBook) {
     let div = document.createElement("div");
     div.className = "card";
-    div.innerHTML =' <p>'+ newBook.title + '</p> <p>'+ newBook.author + '</p>  <p>'+ newBook.nbrPage + '</p> <button class="read"> read</button> <button class="sup">sup</button>'
+    div.innerHTML ='<p>'+ newBook.title + '</p> <p>'+ newBook.author + '</p>  <p>'+ newBook.nbrPage + '</p> '
+    div.innerHTML += (newBook.read)? '</p> <button class="isread green" > read</button>' : '</p> <button class="isread red"> read</button>'
+    div.innerHTML += '<button class="sup">sup</button>'
     displayCards.appendChild(div)
     const deleteButtons = document.querySelectorAll(".sup");
 
@@ -36,7 +40,7 @@ btnAdd.addEventListener("click" , ()=>{
 
 // ajouter une carte (livre)
 form.addEventListener("submit" ,()=>{
-    let hd = new Book(title.value , author.value , nbrPage.value)
+    let hd = new Book(title.value , author.value , nbrPage.value , read.checked)
     addBookToLibrary( hd)
     form.reset()
     dialog.close()
