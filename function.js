efa8a10855882fd41ab7ff7a0f481b304ad4d8a4
Danil gdj <danilguidjou7@gmail.com>
2024-06-14 19:47:55 +0100

    final commit

--- function.js
+++ function.js
@@ -1,70 +1,72 @@
-const btnAdd = document.querySelector("#add")
-const dialog = document.querySelector("dialog")
-const title = document.querySelector("#title")
-const author = document.querySelector("#author")
-const nbrPage = document.querySelector("#nbrPage")
-const read = document.querySelector("#read")
-const exitDialog = document.querySelector("#exit")
-const displayCards = document.querySelector(".displayCard")
-const form = document.querySelector("form")
+const btnAdd = document.querySelector("#add");
+const dialog = document.querySelector("dialog");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const nbrPage = document.querySelector("#nbrPage");
+const read = document.querySelector("#read");
+const exitDialog = document.querySelector("#exit");
+const displayCards = document.querySelector(".displayCard");
+const form = document.querySelector("form");
 
+let allBooks =[];
+
+function reset(){
+    form.reset();
+    dialog.close();
+}
 
 function verifyReadButtons(){
    if(this.classList.contains("green")){
-        this.classList.remove("green")
-        this.classList.add("red")
+        this.classList.remove("green");
+        this.classList.add("red");
     }else{
-        this.classList.remove("red")
-        this.classList.add("green")
+        this.classList.remove("red");
+        this.classList.add("green");
     }  
-    
 }
 
 function Book(title , author , nbrPage, read){
     this.title = title;
     this.author =author;
     this.nbrPage = nbrPage;
-    this.read = read
+    this.read = read;
 }
 
 function addBookToLibrary(newBook) {
     let div = document.createElement("div");
     div.className = "card";
-    div.innerHTML ='<p> Title : '+ newBook.title + '</p> <p> Author : '+ newBook.author + '</p>  <p> number of pages : '+ newBook.nbrPage + '</p> '
-    div.innerHTML += (newBook.read)? '<button class="isread green"  onclick="verifyReadButtons.call(this)"> Read </button>' : '<button class="isread red" onclick="verifyReadButtons.call(this)"> Read</button>'
-    div.innerHTML += '<button class="sup">Remove</button>'
-    displayCards.appendChild(div)
-    
-    const deleteButtons = document.querySelectorAll(".sup");
+    div.innerHTML ='<p> Title : '+ newBook.title + '</p> <p> Author : '+ newBook.author + '</p>  <p> number of pages : '
+                        + newBook.nbrPage + '</p> ';
+    div.innerHTML += (newBook.read)? '<button class="isread green"  onclick="verifyReadButtons.call(this)"> Read </button>' 
+                    : '<button class="isread red" onclick="verifyReadButtons.call(this)"> Read</button>';
+    div.innerHTML += '<button class="sup">Remove</button>';
+    displayCards.appendChild(div);
 
+    const deleteButtons = document.querySelectorAll(".sup");
     deleteButtons.forEach((button)=> {
         button.addEventListener("click",()=>{
-            displayCards.removeChild(button.parentElement)
+            displayCards.removeChild(button.parentElement);
         })
     })
 }
 
-
 btnAdd.addEventListener("click" , ()=>{
-    dialog.showModal()
+    dialog.showModal();
 })
 
 // ajouter une carte (livre)
 form.addEventListener("submit" ,()=>{
-    let hd = new Book(title.value , author.value , nbrPage.value , read.checked)
-    addBookToLibrary( hd)
-    form.reset()
-    dialog.close()
-
+    let newBook = new Book(title.value , author.value , nbrPage.value , read.checked)
+    allBooks.push(newBook);
+    addBookToLibrary( newBook);
+    reset();
 })
 
 //annuler les form
 dialog.addEventListener("close" , ()=>{
-    form.reset()
-    dialog.close()
+    reset();
 })
 
 exitDialog.addEventListener("click" , ()=>{
-    form.reset()
-    dialog.close()
+    reset();
 })
